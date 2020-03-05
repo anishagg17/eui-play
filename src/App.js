402d96c188a24991e64b3e51ce289f19cfc807c5
Anish Aggarwal <anish17122000@gmail.com>
2020-03-05 19:56:43 +0530

    added create page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import "./App.css";
 import Editor from "./components/Editor";
 import NavBar from "./components/NavBar";
 import codes from "./constants/codes";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import store from "./Store";
+import Live from "./components/Live";
 
 function App() {
   return (
-    <Fragment>
-      <NavBar />
-      {codes.map(code => (
-        <Editor code={code} />
-      ))}
-    </Fragment>
+    <Router>
+      <Provider store={store}>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact>
+            <Fragment>
+              {codes.map(code => (
+                <Editor code={code} />
+              ))}
+            </Fragment>
+          </Route>
+          <Route path="/create" exact component={Live} />
+        </Switch>
+      </Provider>
+    </Router>
   );
 }
 
---
+++ src/components/Live.js
@@ -0,0 +1,176 @@
+import React from "react";
+import styled from "styled-components";
+import _ErrorIcon from "@material-ui/icons/Error";
+import { LiveProvider, LiveEditor, withLive } from "react-live";
+
+const Container = styled.div`
+  width: 80vw;
+  margin: 1vh auto;
+  min-height: calc(60vh);
+  font-family: monospace;
+  *:focus {
+    outline: 2px solid rgb(226, 226, 226);
+  }
+  margin-bottom: 5vh;
+  position: relative;
+`;
+
+const Flex = styled.div`
+  width: 100%;
+  display: flex;
+  height: 55vh;
+  * {
+    flex: 1;
+  }
+  positon: absolute;
+`;
+
+const StyledEditor = styled(LiveEditor)`
+  border: 2px solid rgb(51, 51, 51);
+  background-color: rgb(31, 31, 31);
+  color: inherit;
+  box-sizeing: border-box;
+`;
+
+const LivePreview = styled.div`
+  border: 2px solid rgb(51, 51, 51);
+  background-color: rgb(31, 31, 31);
+  //   box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  text-align: center;
+`;
+
+const ErrorIcon = styled(_ErrorIcon)`
+  float: right;
+  // position: absolute;
+  margin: auto 0px;
+`;
+
+const LiveError = styled.div`
+  border: 2px solid rgb(51, 51, 51);
+  height: 20vh;
+  background-color: rgb(31, 31, 31);
+  box-sizing: border-box;
+  padding: 8px;
+  background-color: rgb(84, 26, 20);
+  border: 2px solid rgb(212, 67, 51);
+  color: rgb(226, 226, 226);
+  position: relative;
+`;
+
+const Form = styled.div`
+  border: 2px solid rgb(51, 51, 51);
+  background-color: rgb(31, 31, 31);
+  height: 20vh;
+  display: flex;
+  box-sizing: border-box;
+  align-items: center;
+  color: rgb(226, 226, 226);
+  padding: 5vw;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  border: 2px solid #777;
+  outline: none;
+  align-items: center;
+  min-height: unset;
+  min-width: 140px;
+  padding-right: 16px;
+  padding-left: 16px;
+  background-color: rgb(51, 51, 51);
+  font-family: aktiv-grotesk, sans-serif;
+  color: #ffffff;
+  font-weight: 700;
+  font-size: 14px;
+  height: 4.5vh;
+  line-height: 35px;
+  -webkit-box-pack: center;
+  text-align: center;
+  :hover {
+    cursor: pointer;
+    // border-color: rgb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186);
+    // font: 400 11px system-ui;
+    background-color: rgb(84, 84, 84);
+  }
+`;
+
+const Input = styled.input`
+  border: 2px solid #777;
+  width: 50vw;
+  background: transparent;
+  height: 4.5vh;
+  color: #eee;
+  box-sizing: border-box;
+  padding: 0 10px;
+  ::-webkit-input-placeholder {
+    /* Edge */
+    color: #eee;
+  }
+
+  :-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: #eee;
+  }
+
+  ::placeholder {
+    color: #eee;
+  }
+`;
+
+const TestComponent = ({ live }) => {
+  const Result = live.element;
+  const [desc, UpdateDesc] = React.useState("");
+  // console.log(live);
+  return (
+    <Container>
+      <Flex>
+        <StyledEditor />
+        <LivePreview>
+          <Result />
+        </LivePreview>
+      </Flex>
+      {live.error ? (
+        <LiveError>
+          {live.error}
+          <ErrorIcon />
+        </LiveError>
+      ) : (
+        <Form>
+          <Input
+            placeholder="Add some description"
+            value={desc}
+            onChange={e => UpdateDesc(e.target.value)}
+          ></Input>
+          <Button onClick={() => console.log(desc, live.code)}>Submit</Button>
+        </Form>
+      )}
+    </Container>
+  );
+};
+const LiveComponent = withLive(TestComponent);
+
+const hooksExample = `
+function LikeButton() {
+  const [likes, increaseLikes] = React.useState(0)
+  return (
+    <>
+      <p class="likes">{likes} likes</p>
+      <button
+        class="button"
+        onClick={() => increaseLikes(likes + 1)} />
+    </>
+  )
+}
+`.trim();
+
+export default class Live extends React.Component {
+  render() {
+    return (
+      <LiveProvider code="<div>hello</div>">
+        <LiveComponent />
+      </LiveProvider>
+    );
+  }
+}
